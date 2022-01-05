e8579734f8440a085edc8e9cb5b6a3e74b658b58
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-05 18:20:27 +0530

    Time: 369 ms (5.01%), Space: 166.9 MB (5.15%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str[] = s.split("[\\s@&.?$+-,:#!%_]+");
+        String all = "";
+        for(String i : str){
+            all += i;
+        }
+        all = all.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        int i = 0;
+        int j = all.length()-1;
+        System.out.println(all);
+        while(i<=j){
+            if(all.charAt(i) == all.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
