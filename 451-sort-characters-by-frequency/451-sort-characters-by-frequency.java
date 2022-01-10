a24cb1648cadfb61f099f8e42fb60b24a5045040
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-10 13:23:37 +0530

    Time: 20 ms (38.84%), Space: 45.3 MB (29.77%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -0,0 +1,63 @@
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        for(int i=0;i<s.length(); i++){
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() != 0){
+            char c = pq.poll();
+            int f = map.get(c);
+            for(int i=0; i< f; i++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         HashMap<Character, Integer> map = new HashMap<>();
+//         for(int i=0;i<s.length(); i++){
+//             char c = s.charAt(i);
+//             map.put(c, map.getOrDefault(c, 0)+1);
+//         }
+//         PriorityQueue<Character> p = new PriorityQueue<>((a,b) -> (map.get(a) - map.get(b)));
+//         p.addAll(map.keySet());
+//         StringBuilder sb = new StringBuilder();
+//         while(p.size() != 0){
+//             char c = p.poll();
+//             for(int i=0;i<map.get(c);i++){
+//                 sb.append(c);
+//             }
+//         }
+//         return sb.reverse().toString();
+    }
+}
