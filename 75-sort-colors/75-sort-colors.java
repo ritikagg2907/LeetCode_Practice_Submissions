177307dcfc129d9757ce0540f0cc334f5332896d
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-10 13:09:09 +0530

    Time: 0 ms (100.00%), Space: 38.8 MB (32.90%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,29 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int z = 0;
-        int o = 0;
-        int t = 0;
-        for(int i : nums){
-            if(i == 0){
-                z++;
+        int i=0;
+        int j=nums.length-1;
+        int mid = 0;
+        int temp = 0;
+        while(mid <= j){
+            switch(nums[mid]){
+                case 0:
+                    temp = nums[i];
+                    nums[i] = nums[mid];
+                    nums[mid] = temp;
+                    i++;
+                    mid++;
+                    break;
+                    
+                case 1:
+                    mid++;
+                    break;
+                    
+                case 2:
+                    temp = nums[j];
+                    nums[j] = nums[mid];
+                    nums[mid] = temp;
+                    j--;
+                    break;
             }
-            else if(i==1){
-                o++;
-            }
-            else{
-                t++;
-            }
-        }
-        int i = 0;
-        for(i=0;i<z; i++){
-            nums[i] = 0;
-        }
-        int j = i;
-        for(j=i; j < i+o; j++){
-            nums[j] = 1;
-        }
-        for(int k = j; k<nums.length; k++){
-            nums[k] = 2;
         }
     }
 }
