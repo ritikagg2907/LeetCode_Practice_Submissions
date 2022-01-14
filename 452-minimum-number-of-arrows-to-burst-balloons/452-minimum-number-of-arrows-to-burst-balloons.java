0b2032ad0c1869d988e2702e6fa7761921264fe0
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-14 16:19:31 +0530

    Time: 79 ms (41.05%), Space: 116.4 MB (27.84%) - LeetHub

--- 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
+++ 452-minimum-number-of-arrows-to-burst-balloons/452-minimum-number-of-arrows-to-burst-balloons.java
@@ -1,20 +1,53 @@
 class Solution {
-    public int findMinArrowShots(int[][] points) {
-        if (points == null || points.length == 0) return 0;
-        Arrays.sort(points, (int[] p1, int[] p2)->{
-		    // Don't use p1[1 ] - p2[1]. It may overflow 
+    public int findMinArrowShots(int[][] nums) {
+        
+        Arrays.sort(nums, (int[] p1, int[] p2)->{
             return p1[1] < p2[1] ? -1 : 1; 
         });
-        int end = points[0][1];
-		
-		// the reason res = 1 is that we need an arrow to destroy the last group. 
+        int e = nums[0][1];
         int res = 1;
-        for (int[] point: points)
-        {
-            if (point[0] <= end) continue;
-            res++;
-            end = point[1];
+        for(int arr[] : nums){
+            if(arr[0] > e){
+                res++;
+                e = arr[1];
+            }
         }
         return res;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         if (points == null || points.length == 0) return 0;
+//         Arrays.sort(points, (int[] p1, int[] p2)->{
+// 		    // Don't use p1[1 ] - p2[1]. It may overflow 
+//             return p1[1] < p2[1] ? -1 : 1; 
+//         });
+//         int end = points[0][1];
+		
+// 		// the reason res = 1 is that we need an arrow to destroy the last group. 
+//         int res = 1;
+//         for (int[] point: points)
+//         {
+//             if (point[0] <= end) continue;
+//             res++;
+//             end = point[1];
+//         }
+//         return res;
     }
 }
