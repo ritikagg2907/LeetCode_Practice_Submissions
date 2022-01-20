b892234c22361b98f7225a9bc99f47364ecbf5c4
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-20 13:40:35 +0530

    Time: 17 ms (60.49%), Space: 52.2 MB (22.72%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.java
+++ 875-koko-eating-bananas/875-koko-eating-bananas.java
@@ -1,26 +1,26 @@
 class Solution {
-    public int minEatingSpeed(int[] nums, int h) {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
         int max = 0;
-        for(int i : nums){
+        for(int i : piles){
             max = Math.max(max, i);
         }
-        int s = 1;
-        int e = max;
-        while(s < e){
-            int mid = (e+s)/2;
-            // System.out.println(mid);
-            int t = 0;
-            for(int i : nums){
-                t += Math.ceil((double)i/mid);
-            }
-            if(t <= h){
-                e = mid;
-            }
-            else{
-                s = mid+1;
-            }
+        int right = max;
+        
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(canEatInTime(piles, mid, h)) right = mid - 1;
+            else left = mid + 1;
         }
-        return e;
+        return left;
     }
-    
+    public boolean canEatInTime(int piles[], int k, int h){
+        int hours = 0;
+        for(int pile : piles){
+            int div = pile / k;
+            hours += div;
+            if(pile % k != 0) hours++;
+        }
+        return hours <= h;
+    }
 }
