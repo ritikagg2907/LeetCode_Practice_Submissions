720fb119a7b6c45b3b096824a03540e363b95615
Ritik Aggarwal <55681241+ritikagg2907@users.noreply.github.com>
2022-01-24 20:55:35 +0530

    Time: 9 ms (8.79%), Space: 39.3 MB (14.25%) - LeetHub

--- 520-detect-capital/520-detect-capital.java
+++ 520-detect-capital/520-detect-capital.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean detectCapitalUse(String word) {
-        String ac = word.toUpperCase();
-        String nc = word.toLowerCase();
-        String fc = word.substring(0,1).toUpperCase() + word.substring(1).toLowerCase();
-        if(word.equals(ac) || word.equals(nc) || word.equals(fc)){
-            return true;
-        }
-        return false;
+        return word.matches("[A-Z]*|.[a-z]*");
     }
 }
